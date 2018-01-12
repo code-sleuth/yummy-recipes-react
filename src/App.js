2941c65ba696d95b0136bd890e07f995f7194733
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-12 11:13:37 +0300

    [Feature] Set up react environment and dependencies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="jumbotron">
+       <h1 className="text-center">Yummy Recipes React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
