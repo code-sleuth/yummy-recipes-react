cd70a915a3c25a2977cbed680b23eae6a39ed7f4
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-12 14:56:27 +0300

    [feature] Add information bar. This contains the developers information

---
+++ src/components/common/Info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Info = () => {
+    return(
+        <div className="copyright">
+            <div className="container">
+                <div className="row text-center">
+                    <p>Copyright © 2017 All rights reserved. Ibrahim Mbaziira</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
