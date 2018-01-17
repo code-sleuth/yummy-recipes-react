67cd177d88f74d9cd63bc7cd464cc8d08db409c4
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-17 22:40:51 +0300

    [Feature] Add header component to default app state.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import Navbar from './components/common/Navbar';
-import Info from './components/common/Info';
+import Header from './components/common/Header';
 import Routes from './components/common/routes';
+import Footer from './components/common/Footer';
+import Info from './components/common/Info';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="jumbotron">
-          <h1 className="text-center">Yummy Recipes React App</h1>
-        </div>
-        <Navbar />
+        <Header />
         <Routes />
-        {this.props.children}
+        <Footer />
         <Info />
       </div>
     );
