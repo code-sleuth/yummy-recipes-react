0f7af7d4f328eb792ed84ad5c7f58daea14c4c23
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-17 22:38:45 +0300

    [Chore] Add routes for homepage and signup.

--- src/components/common/routes.js
+++ src/components/common/routes.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import LoginPage from '../login/Login'; 
 import Dashboard from './Dashboard';
-// import Homepage from '../Homepage';
+import SignupPage from '../login/Signup';
+import Homepage from './Homepage';
 
 const Routes = () =>(
     <div>
         <Switch>
-            {/* <Route exact path="/" component={Homepage} /> */}
-            <Route path="/login" component={LoginPage} />
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/login"  component={LoginPage} />
             <Route path="/dashboard" component={Dashboard} />
+            <Route exact path="/signup"  component={SignupPage} />
         </Switch>
     </div>
 );
