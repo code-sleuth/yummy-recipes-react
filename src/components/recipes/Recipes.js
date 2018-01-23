794b773bdbb332499e7f816a7838853bc1306b0b
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-23 16:57:36 +0300

    [Feature] Use table to display users current recipes

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -16,6 +16,7 @@ class Recipes extends Component {
     componentDidMount(){
         axios.get('http://127.0.0.1:5000/recipes', {headers: {Authorization: this.getAuthenticationToken()}})
         .then(response => {
+            console.log(this.getAuthenticationToken())
             console.log(response.data)
             this.setState({recipes: response.data})
         })
@@ -37,36 +38,40 @@ class Recipes extends Component {
     }
 
     render(){
-        //console.log(localStorage.getItem('token'))
+        console.log(this.state.recipes)
         if (this.state.recipes){
         const rec_ipes = this.state.recipes.map((recipes) => {
             return(
-                <div className="col-xs-12 col-sm-6 col-md-3" key={recipes.id}>
-                    <div className="thumbnail">
-                        <img src={RecipeImage} />
-                        <div className="caption">
-                            <h3>{recipes.category_id}</h3>
-                            <h4 className="text-uppercase">{recipes.name}</h4>
-                            <p className="text-justify">{recipes.details}</p>
-                            <h5>Ingredients:</h5>
-                            <ul>
-                                <li>{recipes.ingredients}</li>
-                            </ul>
-
-                            <hr />
-                            <p className="small"><em>Created: {recipes.date_created}</em></p>
-                            <p className="small"><em>Last Modified: {recipes.date_modified}</em></p>
-                        </div>
-                    </div>
-                </div>
+                <tbody>
+                <tr>
+                    <td>{recipes.category_id}</td>
+                    <td>{recipes.name}</td>
+                    <td>{recipes.details}</td>
+                    <td>{recipes.ingredients}</td>
+                    <td>{recipes.date_created}</td>
+                    <td>{recipes.date_modified}</td>
+                </tr>
+                </tbody>
             );
         });
 
         return(
-            <div className="container">
-                <div className="row text-left">
-                { rec_ipes }
-                </div>
+            <div class="container">
+                <h2>Recipes</h2>
+                <p>A list of all created Recipes</p>                              
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th>Category Name</th>
+                        <th>Recipe Name</th>
+                        <th>Details</th>
+                        <th>Ingredients</th>
+                        <th>Date Created</th>
+                        <th>Last Edited</th>
+                    </tr>
+                    </thead>
+                    { rec_ipes }
+                </table>
             </div>
         );
     }
