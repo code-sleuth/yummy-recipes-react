e0203570e3d21ae98f04153537a4e3c6b7d6816a
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-25 22:37:13 +0300

    [Feature] Add pagination to returned recipes

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -70,6 +70,15 @@ class Recipes extends Component {
                     </thead>
                     { rec_ipes }
                 </table>
+                <div class="text-center">
+                        <ul class="pagination">
+                            <li><a href="#">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">4</a></li>
+                            <li><a href="#">5</a></li>
+                        </ul>
+                </div>
             </div>
         );
     }
