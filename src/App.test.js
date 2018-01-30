420e2a87dc8ab202e98045513fdc16037316c7c7
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-30 21:57:27 +0300

    [Chore] Implement testing main app using enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  , div);
+  shallow(<App />);
 });
