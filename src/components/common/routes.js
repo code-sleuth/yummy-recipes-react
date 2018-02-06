cc3e447cea7067efad3056304ee00417e3c77063
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-02-06 23:40:32 +0300

    [Chore] Update routes

--- src/components/common/routes.js
+++ src/components/common/routes.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import LoginPage from '../login/Login'; 
+import LoginForm from '../login/LoginForm'; 
 import Dashboard from './Dashboard';
 import SignupPage from '../login/Signup';
 import Homepage from './Homepage';
 import Category from '../categories/Category';
-import Recipe from '../recipes/Recipe';
 import User from '../user/User';
+import PrivateRoute from '../../utils/PrivateRoute';
+import EditCategory from '../categories/EditCategory';
+import AddCategory from '../categories/AddCategory';
+import EditRecipe from '../recipes/EditRecipe';
+import AddRecipe from '../recipes/AddRecipe';
 
 const Routes = () =>(
     <div>
         <Switch>
             <Route exact path="/" component={Homepage} />
-            <Route exact path="/login"  component={LoginPage} />
-            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/login"  component={LoginForm} />
             <Route exact path="/signup"  component={SignupPage} />
-            <Route exact path="/dashboard/categories" component={Category} /> 
-            <Route exact path="/dashboard/recipes" component={Recipe} />
-            <Route exact path="/dashboard/user" component={User} />
+            <PrivateRoute path="/recipe/:id" component={Dashboard} />
+            <PrivateRoute path="/add_recipe" component={AddRecipe} />
+            <PrivateRoute path="/edit_recipe/:id" component={EditRecipe} />
+            <PrivateRoute path="/categories" component={Category} /> 
+            <PrivateRoute path="/edit_categories/:id" component={EditCategory} /> 
+            <PrivateRoute exact path="/add_categories" component={AddCategory} /> 
+            <PrivateRoute exact path="/user" component={User} />
         </Switch>
     </div>
 );
