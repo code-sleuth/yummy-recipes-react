dbc90ad294593856c1f430d583c5c225135c0569
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-02-06 23:35:35 +0300

    [Feature] Remove useless controls from add recipe component

--- src/components/recipes/AddRecipe.js
+++ src/components/recipes/AddRecipe.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {BASE_URL, AuthToken} from '../../utils/Constants';
+import Navbar from '../common/Navbar';
 
 class AddRecipe extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
             category_array: [],
             name: '',
             details: '',
             ingredients: '',
-            category_id: '',
-            created_by: 1
+            category_id: ''
         }
     }
 
-    getAccessToken(){
-        return localStorage.getItem('token')
+    getRecipeId(){
+        return this.props.match.params.id;
     }
 
     componentDidMount(){
-        axios.get('http://127.0.0.1:5000/categories', {headers: {Authorization: this.getAccessToken()}})
+        axios.get(BASE_URL+'categories/combo', {headers: {Authorization: AuthToken}})
         .then(response => {
             this.setState({category_array: response.data})
         })
@@ -30,7 +31,7 @@ class AddRecipe extends Component{
 
     handleSubmit = (event) => {
         event.preventDefault();
-        axios.post('http://127.0.0.1:5000/recipes',
+        axios.post(BASE_URL+'recipes',
          {
             category_id: this.state.category_id,
             name: this.state.name,
@@ -38,10 +39,12 @@ class AddRecipe extends Component{
             ingredients: this.state.ingredients,
             created_by: this.state.created_by
         },
-        {headers: {Authorization: this.getAccessToken()}})
+        {headers: {Authorization: AuthToken}})
         .then(response => {
-            console.log(response.data);
-            alert('Success')
+            console.log('The idea is:' + this.getRecipeId())
+            alert('Successfully Added Recipe ', this.getRecipeId())
+            this.setState()
+            this.props.history.push('/categories')
         })
         .catch(error => {
             alert(error);
@@ -66,91 +69,59 @@ class AddRecipe extends Component{
     }
 
     render(){
+        console.log(this.getRecipeId())
         if(this.state.category_array){
-            const all_recipes = this.state.category_array.map((category) => {
+            const all_categories = this.state.category_array.map((category) => {
                 return(
                     <option value={category.id} key={category.id}>{category.name}</option>
                 );
             })
         
             return(
+                <div>
+                    <Navbar />
                 <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h4 className="modal-title">YUMMY RECIPES: Add Recipe</h4>
-                            </div>
-                            <div className="modal-body">
-                                <form onSubmit={this.handleSubmit}>
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="glyphicon glyphicon-plus"></i></span>                                        
-                                        <select className="form-control" name="category" onChange={this.handleSelectChanged} value={this.state.category_id} required>
-                                            <option value={0}>SELECT CATEGORY TO ATTACH</option>
-                                            { all_recipes }
-                                        </select>
-                                    </div>
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="glyphicon glyphicon-pencil"></i></span>
-                                        <input type="text" name="recipename" placeholder="RECIPE NAME" className="form-control"
-                                         onChange={this.handleRecipeNameChanged}
-                                         required />
-                                    </div>
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="glyphicon glyphicon-list-alt"></i></span>
-                                        <textarea type="text" name="description" placeholder="RECIPE DESCRIPTION" className="form-control" 
-                                        onChange={this.handleDetailsChanged}
-                                        required></textarea>
-                                    </div>
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="glyphicon glyphicon-glass"></i></span>
-                                        <textarea type="text" name="ingredients" placeholder="RECIPE INGREDIENTS" className="form-control"
-                                        onChange={this.handleIngredientsChanged}
-                                        required></textarea>
-                                    </div>
-                                    <br />
-                                    <div className="form-group text-center">
-                                        <input className="btn btn-success btn-md" type="submit" value="CREATE RECIPE" />
-                                    </div>
-                                </form>
-                            </div>
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h4 className="modal-title">YUMMY RECIPES: Add Recipe</h4>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="input-group">
+                                    <span className="input-group-addon"><i className="glyphicon glyphicon-plus"></i></span>                                        
+                                    <select className="form-control" name="category" onChange={this.handleSelectChanged} value={this.state.category_id} required>
+                                        <option value={0}>SELECT CATEGORY TO ATTACH</option>
+                                        { all_categories }
+                                    </select>
+                                </div>
+                                <div className="input-group">
+                                    <span className="input-group-addon"><i className="glyphicon glyphicon-pencil"></i></span>
+                                    <input type="text" name="recipename" placeholder="RECIPE NAME" className="form-control"
+                                        onChange={this.handleRecipeNameChanged}
+                                        required />
+                                </div>
+                                <div className="input-group">
+                                    <span className="input-group-addon"><i className="glyphicon glyphicon-list-alt"></i></span>
+                                    <textarea type="text" name="description" placeholder="RECIPE DESCRIPTION" className="form-control" 
+                                    onChange={this.handleDetailsChanged}
+                                    required></textarea>
+                                </div>
+                                <div className="input-group">
+                                    <span className="input-group-addon"><i className="glyphicon glyphicon-glass"></i></span>
+                                    <textarea type="text" name="ingredients" placeholder="RECIPE INGREDIENTS" className="form-control"
+                                    onChange={this.handleIngredientsChanged}
+                                    required></textarea>
+                                </div>
+                                <br />
+                                <div className="form-group text-center">
+                                    <input className="btn btn-success btn-md" type="submit" value="CREATE RECIPE" />
+                                </div>
+                            </form>
                         </div>
                     </div>
-                );
-            } else {
-                return(
-                <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h4 className="modal-title">YUMMY RECIPES: Add Recipe</h4>
-                    </div>
-                    <div className="modal-body">
-                        <form action="/add_recipe" method="POST">
-                            <div className="input-group">
-                                <span className="input-group-addon"><i className="glyphicon glyphicon-plus"></i></span>                                        
-                                <select className="form-control" name="category" required="required">
-                                    <option value="">SELECT CATEGORY TO ATTACH</option>
-                                </select>
-                            </div>
-                            <div className="input-group">
-                                <span className="input-group-addon"><i className="glyphicon glyphicon-pencil"></i></span>
-                                <input type="text" name="recipename" placeholder="RECIPE NAME" className="form-control" id="recipename" required />
-                            </div>
-                            <div className="input-group">
-                                <span className="input-group-addon"><i className="glyphicon glyphicon-list-alt"></i></span>
-                                <textarea type="text" name="description" placeholder="RECIPE DESCRIPTION" className="form-control" id="description" required></textarea>
-                            </div>
-                            <div className="input-group">
-                                <span className="input-group-addon"><i className="glyphicon glyphicon-glass"></i></span>
-                                <textarea type="text" name="ingredients" placeholder="RECIPE INGREDIENTS" className="form-control" id="ingredients"  required></textarea>
-                            </div>
-                            <br />
-                            <div className="form-group text-center">
-                                <input className="btn btn-success btn-md" type="submit" name="submitrecipe" value="CREATE RECIPE" />
-                            </div>
-                        </form>
-                    </div>
                 </div>
-            </div>   
-            );    
+            </div>
+            );
         }
     }
 }
