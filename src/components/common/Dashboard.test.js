efa53e50e0b5a9cb52317fcf832cad69f8daa054
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-02-09 17:17:10 +0300

    [Chore] Test more dashboard components

--- src/components/common/Dashboard.test.js
+++ src/components/common/Dashboard.test.js
@@ -2,22 +2,29 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Navbar from './Navbar';
 import Recipes from '../recipes/Recipes';
+import Dashboard from './Dashboard';
 
 describe('these are category component tests', () => {
     const props={
-        params:{
-            match:{
+        match:{
+            params:{
+
             }
         } 
     }
-    it('renders AddCategory component', () => {
+    it('renders  navbar', () => {
         const wrapper = shallow(<Navbar />)
         const header = <a className='navbar-brand'>Yummy Recipes</a>
         expect(wrapper.contains(header)).toEqual(true)
     });
 
-    // it('renders Recipes component', () => {
-    //     const wrapper = shallow(<Recipes {...props}/>)
-    //     expect(wrapper.find('h2').length).toBe(12)
-    // });
+    it('has navbar', () => {
+        const wrapper = shallow(<Dashboard {...props}/>)
+        expect(wrapper.find('Navbar').length).toBe(1)
+    })
+
+    it('has div', () => {
+        const wrapper = shallow(<Dashboard {...props}/>)
+        expect(wrapper.find('div').length).toBe(1)
+    })
 });
