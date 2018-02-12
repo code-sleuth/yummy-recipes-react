5c239ed00e158e8b2a12054780fafd0f1804c9ec
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-02-12 22:18:33 +0300

    [Chore] Use one function to handle user input

--- src/components/recipes/EditRecipe.js
+++ src/components/recipes/EditRecipe.js
@@ -8,11 +8,10 @@ class EditRecipe extends Component{
         this.state = {
             category_array: [],
             recipe_array: [],
-            recipe_id: '',
-            category_id: '',
-            name: '',
+            recData:{name: '',
             details: '',
-            ingredients: ''
+            ingredients: ''},
+            category_id: ''
         }
     }
 
@@ -27,40 +26,23 @@ class EditRecipe extends Component{
         })
     }
 
-    // function to handle recipe input changed
-    handleRecipeChanged = (event) => {
-        this.setState({recipe_id: event.target.value});
-    }
-
-    // function to handle category input changed
-    handleCategoryChanged = (event) => {
-        this.setState({category_id: event.target.value});
-    }
-
-    // function to handle recipe name input changed
-    handleRecipeNameChanged = (event) => {
-        this.setState({name: event.target.value});
-    }
-
-    // function to handle recipe detail input changed
-    handleRecipeDetailsChanged = (event) => {
-        this.setState({details: event.target.value});
-    }
-
-    // function to handle recipe ingredients input changed
-    handleRecipeIngredientsChanged = (event) => {
-        this.setState({ingredients: event.target.value});
+    //function to handle user input
+    handleChange = (event) => {
+        const field = event.target.id
+        let recData = this.state.recData
+        recData[field] = event.target.value
+        this.setState({ recData })
     }
 
     // function to handle user submitted data
     handleSubmit = (event) => {
         event.preventDefault();
-        const {category_id, name, details, ingredients} = this.state
+        const {recData, category_id} = this.state
         axios.put(`${BASE_URL}recipes/${this.getId()}`, {
-            category_id: category_id,
-            name: name,
-            details: details,
-            ingredients: ingredients
+            category_id: category_id, 
+            name: recData.name,
+            details: recData.details,
+            ingredients: recData.ingredients
         },
         {headers: {Authorization: AuthToken}})
         .then(response => {
@@ -92,20 +74,20 @@ class EditRecipe extends Component{
                             <form onSubmit={this.handleSubmit} >
                                 <div className="input-group">
                                     <span className="input-group-addon"><i className="glyphicon glyphicon-pencil"></i></span>
-                                    <input type="text" placeholder={"OLD RECIPE NAME: "+recipe_array.name} className="form-control" id="recipe_name"
-                                    onChange={this.handleRecipeNameChanged}
+                                    <input type="text" placeholder={"OLD RECIPE NAME: "+recipe_array.name} className="form-control" id="name"
+                                    onChange={this.handleChange}
                                     required />
                                 </div>
                                 <div className="input-group">
                                     <span className="input-group-addon"><i className="glyphicon glyphicon-list-alt"></i></span>
-                                    <textarea type="text" name="new_description" placeholder={"OLD DETAILS: "+recipe_array.details} className="form-control"
-                                    onChange={this.handleRecipeDetailsChanged}
+                                    <textarea type="text" id="details" placeholder={"OLD DETAILS: "+recipe_array.details} className="form-control"
+                                    onChange={this.handleChange}
                                     required></textarea>
                                 </div>
                                 <div className="input-group">
                                     <span className="input-group-addon"><i className="glyphicon glyphicon-glass"></i></span>
-                                    <textarea type="text" name="new_ingredients" placeholder={"OLD INGREDIENTS: "+recipe_array.ingredients} className="form-control"
-                                    onChange={this.handleRecipeIngredientsChanged}
+                                    <textarea type="text" id="ingredients" placeholder={"OLD INGREDIENTS: "+recipe_array.ingredients} className="form-control"
+                                    onChange={this.handleChange}
                                     required></textarea>
                                 </div>
                                 <br />
